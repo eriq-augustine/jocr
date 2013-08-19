e811f676a2ae9a365cb8d1cd71f7a27c55cc6529
Eriq Augustine <eaugusti@chromium.org>
2013-08-19 01:23:12 -0700

    Working on slice line detection.

--- src/com/eriqaugustine/ocr/image/CharacterImage.java
+++ src/com/eriqaugustine/ocr/image/CharacterImage.java
@@ -5,6 +5,10 @@ import com.eriqaugustine.ocr.utils.ImageUtils;
 import com.eriqaugustine.ocr.utils.MathUtils;
 
 import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import magick.MagickImage;
 
@@ -14,6 +18,8 @@ import magick.MagickImage;
 public class CharacterImage {
    public static final int DEFAULT_POINT_SIZE = 2;
    public static final double DEFAULT_POINT_DENSITY = 0.75;
+
+   private static final double DEFAULT_OVERLAP_PERCENT = 0.50;
 
    /**
     * Break up the character into strokes.
@@ -25,8 +31,10 @@ public class CharacterImage {
       byte[] pixels = Filters.averageChannels(Filters.bwPixels(image, 200), 3);
       System.out.println(ImageUtils.asciiImage(pixels, dimensions.width, 1) + "\n");
 
+      /*
       boolean[] points = discretizeLines(pixels, dimensions.width);
       System.out.println(ImageUtils.asciiImage(points, dimensions.width / DEFAULT_POINT_SIZE) + "\n");
+      */
 
       /*
       boolean[] outline = getOutline(pixels, dimensions.width);
@@ -38,7 +46,190 @@ public class CharacterImage {
       System.out.println(ImageUtils.asciiImage(points, dimensions.width) + "\n");
       */
 
+      List<List<int[]>> hSlices = horizontalCharacterSlices(pixels, dimensions.width);
+      System.err.println("Horizontal Slices:");
+      for (int row = 0; row < hSlices.size(); row++) {
+         for (int slice = 0; slice < hSlices.get(row).size(); slice++) {
+            System.err.print(String.format("   [%d, %d]",
+                                           hSlices.get(row).get(slice)[0],
+                                           hSlices.get(row).get(slice)[1]));
+         }
+         System.err.println();
+      }
+      System.err.println();
+
+      List<List<int[]>> lines = slicesToLines(hSlices);
+      System.err.println("Vertical Lines:");
+      for (int i = 0; i < lines.size(); i++) {
+         System.err.println("   Line: " + i);
+
+         for (int[] bounds : lines.get(i)) {
+            System.err.println(String.format("      [%d, %d]", bounds[0], bounds[1]));
+         }
+      }
+
+      /*
+      List<List<int[]>> vSlices = verticalCharacterSlices(pixels, dimensions.width);
+      System.err.println("Vertical Slices:");
+      for (int row = 0; row < vSlices.size(); row++) {
+         for (int slice = 0; slice < vSlices.get(row).size(); slice++) {
+            System.err.print(String.format("   [%d, %d]",
+                                           vSlices.get(row).get(slice)[0],
+                                           vSlices.get(row).get(slice)[1]));
+         }
+         System.err.println();
+      }
+      System.err.println();
+      */
+
       // TODO(eriq).
+   }
+
+   public static List<List<int[]>> slicesToLines(List<List<int[]>> slices) {
+      return slicesToLines(slices, DEFAULT_OVERLAP_PERCENT);
+   }
+
+   public static List<List<int[]>> slicesToLines(List<List<int[]>> slices, double overlapPercent) {
+      List<List<int[]>> lines = new ArrayList<List<int[]>>();
+
+      Set<Integer> usedLineParts = new HashSet<Integer>();
+
+      // Lines that are not yet complete.
+      // A line becomes complete either at the end, or when it was not used in a slice.
+      List<List<int[]>> lineParts = new ArrayList<List<int[]>>();
+
+      // The line parts that were added in the current slice.
+      // These are NOT parts that were appended to.
+      List<List<int[]>> newLineParts = new ArrayList<List<int[]>>();
+
+      for (int i = 0; i < slices.size(); i++) {
+         for (int[] bounds : slices.get(i)) {
+            if (i == 0) {
+               List<int[]> newLine = new ArrayList<int[]>();
+               newLine.add(bounds);
+               newLineParts.add(newLine);
+            } else {
+               boolean append = false;
+
+               for (int linePartIndex = 0; linePartIndex < lineParts.size(); linePartIndex++) {
+                  List<int[]> linePart = lineParts.get(linePartIndex);
+
+                  if (absoluteOverlapPercent(bounds, linePart.get(linePart.size() - 1)) >=
+                      overlapPercent) {
+                     linePart.add(bounds);
+                     usedLineParts.add(new Integer(linePartIndex));
+                     append = true;
+                     break;
+                  }
+               }
+
+               if (!append) {
+                  List<int[]> newLine = new ArrayList<int[]>();
+                  newLine.add(bounds);
+                  newLineParts.add(newLine);
+               }
+            }
+         }
+
+         // Remove all the unused line parts from the list.
+         // Must be kept in order.
+         List<Integer> toRemove = new ArrayList<Integer>();
+         for (int j = 0; j < lineParts.size(); j++) {
+            if (!usedLineParts.contains(new Integer(j))) {
+               toRemove.add(new Integer(j));
+            }
+         }
+
+         for (int j = toRemove.size() - 1; j >= 0; j--) {
+            lines.add(lineParts.remove(toRemove.get(j).intValue()));
+         }
+
+         // Move the new line parts to the formal list.
+         lineParts.addAll(newLineParts);
+         newLineParts.clear();
+      }
+
+      // Add all the final line parts.
+      lines.addAll(lineParts);
+
+      return lines;
+   }
+
+   /**
+    * Given two bounds ([start, end]), give the absolute percentage that they overlap.
+    * For the ratio, the larger of the two bounds will be used.
+    */
+   private static double absoluteOverlapPercent(int[] a, int[] b) {
+      assert(a[0] < a[1]);
+      assert(b[0] < b[1]);
+
+      int count = 0;
+      for (int i = a[0]; i <= a[1]; i++) {
+         if (i >= b[0] && i <= b[1]) {
+            count++;
+         }
+      }
+
+      return (double)count / Math.max(a[1] - a[0] + 1, b[1] - b[0] + 1);
+   }
+
+   /**
+    * Take an image of a character, and slice that image horizontally.
+    * Return an array of indexes that represent the start and end of a section
+    * of the character in that row (slice).
+    * The return would be a List<int[]>[] (to keep with the convention that known sizes
+    * are arrays and not lists), but java generic array creation blocks that.
+    */
+   public static List<List<int[]>> horizontalCharacterSlices(byte[] pixels, int imageWidth) {
+      List<List<int[]>> bounds = new ArrayList<List<int[]>>();
+
+      for (int row = 0; row < pixels.length / imageWidth; row++) {
+         bounds.add(new ArrayList<int[]>());
+
+         int boundStart = -1;
+         for (int col = 0; col < imageWidth; col++) {
+            int index = MathUtils.rowColToIndex(row, col, imageWidth);
+
+            if ((0xFF & pixels[index]) == 0 && boundStart == -1) {
+               boundStart = col;
+            } else if ((0xFF & pixels[index]) != 0 && boundStart != -1) {
+               bounds.get(row).add(new int[]{boundStart, col - 1});
+               boundStart = -1;
+            }
+         }
+
+         if (boundStart != -1) {
+            bounds.get(row).add(new int[]{boundStart, imageWidth - 1});
+         }
+      }
+
+      return bounds;
+   }
+
+   public static List<List<int[]>> verticalCharacterSlices(byte[] pixels, int imageWidth) {
+      List<List<int[]>> bounds = new ArrayList<List<int[]>>();
+
+      for (int col = 0; col < imageWidth; col++) {
+         bounds.add(new ArrayList<int[]>());
+
+         int boundStart = -1;
+         for (int row = 0; row < pixels.length / imageWidth; row++) {
+            int index = MathUtils.rowColToIndex(row, col, imageWidth);
+
+            if ((0xFF & pixels[index]) == 0 && boundStart == -1) {
+               boundStart = row;
+            } else if ((0xFF & pixels[index]) != 0 && boundStart != -1) {
+               bounds.get(col).add(new int[]{boundStart, row - 1});
+               boundStart = -1;
+            }
+         }
+
+         if (boundStart != -1) {
+            bounds.get(col).add(new int[]{boundStart, pixels.length / imageWidth - 1});
+         }
+      }
+
+      return bounds;
    }
 
    /**

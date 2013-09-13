499d11569e8171ac4d780fde4c20c58cd39a201b
Eriq Augustine <eaugusti@chromium.org>
2013-09-12 21:04:03 -0700

    Added Props and WebUtils from some other projects.

---
+++ src/com/eriqaugustine/ocr/utils/Props.java
@@ -0,0 +1,258 @@
+package com.eriqaugustine.ocr.utils;
+
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * The Props is the place to go for properties and configurations.
+ * Props will read any any specified files and hold them in a Map.
+ * You cannot remove properties.
+ * If there are properties in multiple files, the later properties will override the first.
+ *
+ * Advanced Apache Commons logging are used, so you can use the array grouping feature.
+ *  Just give everything the same name, and they will be grouped as a List<String>.
+ *
+ * Properties are held only as Strings or List<String>.
+ * However, you can request properties in specified formats.
+ * If the conversion fails, you will get the default value.
+ */
+public class Props {
+   /**
+    * All the properties read in.
+    */
+   private static Map<String, PropType> props = new HashMap<String, PropType>();
+
+   // TODO(eriq): This should really be done by the primary driver
+   static {
+      readFile("config/config.properties");
+   }
+
+   /**
+    * Check to see if the key has an associated property.
+    */
+   public static boolean has(String key) {
+      return props.containsKey(key);
+   }
+
+   /**
+    * Get a String value.
+    *
+    * @param key The key.
+    *
+    * @return the value associated with key, or null if the key
+    *  does not exist.
+    */
+   public static String getString(String key) {
+      PropType prop = props.get(key);
+
+      if (prop == null || !prop.isString()) {
+         // TODO(eriq): Log warn
+         System.err.println("Tried to get a property that does not exist: " + key);
+         return null;
+      }
+
+      return ((StringProp)prop).data;
+   }
+
+   /**
+    * Get a String value, or a default value if key is not in the properties.
+    *
+    * @param key The key.
+    * @param defaultValue The value that will be returned if key does not exist.
+    *
+    * @return the value associated with key, or defaultValue.
+    */
+   public static String getString(String key, String defaultValue) {
+      String val = getString(key);
+
+      if (val == null) {
+         return defaultValue;
+      }
+
+      return val;
+   }
+
+   /**
+    * Get a int value, or a default value if key is not in the properties.
+    *
+    * @param key The key.
+    * @param defaultValue The value that will be returned if key does not exist.
+    *
+    * @return the value associated with key, or defaultValue.
+    */
+   public static int getInt(String key, int defaultValue) {
+      String val = getString(key);
+
+      if (val == null) {
+         return defaultValue;
+      }
+
+      return Integer.parseInt(val);
+   }
+
+   /**
+    * Get a double value, or a default value if key is not in the properties.
+    *
+    * @param key The key.
+    * @param defaultValue The value that will be returned if key does not exist.
+    *
+    * @return the value associated with key, or defaultValue.
+    */
+   public static double getDouble(String key, double defaultValue) {
+      String val = getString(key);
+
+      if (val == null) {
+         return defaultValue;
+      }
+
+      return Double.parseDouble(val);
+   }
+
+   /**
+    * Get a boolean value, or a default value if key is not in the properties.
+    * WARNING: There is no check to see if the value in the store is "true" or "false".
+    *  Anything that is not "true" evaluates to false.
+    *
+    * @param key The key.
+    * @param defaultValue The value that will be returned if key does not exist.
+    *
+    * @return the value associated with key, or defaultValue.
+    */
+   public static boolean getBoolean(String key, boolean defaultValue) {
+      String val = getString(key);
+
+      if (val == null) {
+         return defaultValue;
+      }
+
+      return Boolean.parseBoolean(val);
+   }
+
+   /**
+    * Get a list of string values.
+    * If the key does not exists, then an empty list will be
+    *  returned.
+    * If the key is not a list, then the value will be put in a list.
+    *
+    * @param key The key.
+    *
+    * @return the value associated with key, or an empty list.
+    */
+   public static List<String> getList(String key) {
+      List<String> rtn = new ArrayList<String>();
+
+      PropType prop = props.get(key);
+
+      if (prop == null) {
+         return rtn;
+      }
+
+      if (!prop.isArray()) {
+         rtn.add(((StringProp)prop).data);
+      } else {
+         for (String strProp : ((ListProp)prop).data) {
+            rtn.add(strProp);
+         }
+      }
+
+      return rtn;
+   }
+
+   /**
+    * Read a properties file and load all of the properties into Props.
+    * Any properties conflicting with previous files will be overwritten.
+    */
+   public static boolean readFile(String fileName) {
+      //Note: getKeys() returns the SET of all keys.
+      // Therefore, lists will only be listed once.
+
+      //Every key will be attempted to be lifted as an array.
+      // If the array only has one element, it will be turned into a
+      // single value.
+      boolean failedConversion = false;
+
+      try {
+         PropertiesConfiguration propsFile = new PropertiesConfiguration(fileName);
+
+         @SuppressWarnings("unchecked")
+         Iterator<String> keys = propsFile.getKeys();
+         while (keys.hasNext()) {
+            String key = keys.next();
+
+            try {
+               String[] vals = propsFile.getStringArray(key);
+               PropType prop;
+
+               if (vals.length == 1) {
+                  prop = new StringProp(vals[0]);
+               } else {
+                  List<String> propList = new ArrayList<String>();
+                  for (String val : vals) {
+                     propList.add(val);
+                  }
+                  prop = new ListProp(propList);
+               }
+               props.put(key, prop);
+            } catch (Exception ex) {
+               // TODO(eriq): Log Error
+               System.err.println("Error lifting property, [" + key +
+                "] from file: " + fileName + ".");
+               failedConversion = true;
+            }
+         }
+      } catch (Exception ex) {
+         // TODO(eriq): Log Error
+         System.err.println("Error getting properties from: " + fileName + ".");
+         return false;
+      }
+
+      return !failedConversion;
+   }
+
+   /**
+    * The types of properties allowed.
+    * This is either a String or List<String>.
+    * Doing this avoids having to do weird/unsafe casting.
+    */
+   private static class PropType {
+      /**
+       * If it is not a String, it is an Array<String>.
+       */
+      private boolean isString;
+
+      public PropType(boolean isString) {
+         this.isString = isString;
+      }
+
+      public boolean isString() {
+         return isString;
+      }
+
+      public boolean isArray() {
+         return !isString;
+      }
+   }
+
+   private static class StringProp extends PropType {
+      public String data;
+
+      public StringProp(String data) {
+         super(true);
+         this.data = data;
+      }
+   }
+
+   private static class ListProp extends PropType {
+      public List<String> data;
+
+      public ListProp(List<String> data) {
+         super(false);
+         this.data = data;
+      }
+   }
+}
---
+++ src/com/eriqaugustine/ocr/utils/WebUtils.java
@@ -0,0 +1,189 @@
+package com.eriqaugustine.ocr.utils;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.FileUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.nio.charset.UnsupportedCharsetException;
+
+/**
+ * A class for general web centered utilities.
+ */
+public class WebUtils {
+   private final static String WEB_CACHE_PREFIX = "web_cache";
+
+   /**
+    * Testing main.
+    */
+   public static void main(String[] args) {
+      String apiKey = Props.getString("GOOGLE_API_KEY");
+      String targetLang = "ja";
+      String targetText = "Hello%20world";
+
+      if (apiKey == null) {
+         System.err.println("NO KEY: GOOGLE_API_KEY");
+      }
+
+      String url = String.format("https://www.googleapis.com/language/translate/v2?" +
+                                 "key=%s&source=en&target=%s&q=%s",
+                                 apiKey, targetLang, targetText);
+
+      String page = fetchPageAsString(url, false);
+      System.out.println(page);
+   }
+
+   public static Reader fetchPageAsReader(String address) {
+      return fetchPageAsReader(address, true);
+   }
+
+   public static Reader fetchPageAsReader(String address, boolean cache) {
+      final String page = fetchPageImpl(address, cache);
+      if (page != null) {
+         return new StringReader(page);
+      }
+
+      return null;
+   }
+
+   public static String fetchPageAsString(String address) {
+      return fetchPageAsString(address, true);
+   }
+
+   public static String fetchPageAsString(String address, boolean cache) { 
+      return fetchPageImpl(address, cache);
+   }
+
+   /**
+    * Get a web page.
+    * TODO(eriq): Don't just error on all non-200 status and return the status.
+    * @return A Reader that the caller owns on success, null on failure.
+    */
+   private static String fetchPageImpl(String address, boolean cache) {
+      String page = null;
+      File cacheFile = null;
+      final URL url;
+
+      try {
+         url = new URL(address);
+      } catch (java.net.MalformedURLException ex) {
+         // TODO(eriq): Log all these errors.
+         ex.printStackTrace(System.err);
+         return null;
+      }
+
+      // Check the cache for the page.
+      if (cache) {
+         cacheFile = getCacheFile(url);
+         page = fetchCache(cacheFile);
+         if (page != null) {
+            return page;
+         }
+      }
+
+      final URLConnection conn;
+      try {
+         conn = url.openConnection();
+      } catch (Exception ex) {
+         ex.printStackTrace(System.err);
+         return null;
+      }
+
+      final String encoding = conn.getContentEncoding();
+
+      Charset charset = Charset.forName("UTF-8");
+      if (encoding != null) {
+         try {
+            charset = Charset.forName(encoding);
+         } catch (UnsupportedCharsetException e) {
+            // Just use the default encoding.
+         }
+      }
+
+      try {
+         final InputStream is = conn.getInputStream();
+         final InputStreamReader reader = new InputStreamReader(is, charset);
+         page = readerToString(reader);
+      } catch (IOException ioEx) {
+         ioEx.printStackTrace(System.err);
+         return null;
+      }
+
+      // Put in the cache
+      if (cache) {
+         if (page != null) {
+            putCache(cacheFile, page);
+         }
+      }
+
+      return page;
+   }
+
+   private static String readerToString(Reader reader) {
+      String rtn = "";
+
+      if (reader == null) {
+         return null;
+      }
+
+      BufferedReader bufferedReader = null;
+      try {
+         bufferedReader = new BufferedReader(reader);
+         String line = null;
+         while ((line = bufferedReader.readLine()) != null) {
+            rtn += line + "\n";
+         }
+      } catch (Exception ex) {
+         return null;
+      } finally {
+         try {
+            if (bufferedReader != null) {
+               bufferedReader.close();
+            }
+            reader.close();
+         } catch (Exception ex) {
+         }
+      }
+
+      return rtn;
+   }
+
+   private static File getCacheFile(URL url) {
+      final String hash = DigestUtils.sha1Hex(url.toString());
+      final String cachePath = Props.getString("CACHE_DIR") + File.separator +
+                               WEB_CACHE_PREFIX + "_" + hash;
+      return new File(cachePath);
+   }
+
+   private static void putCache(File cacheFile, String page) {
+      try {
+         FileUtils.writeStringToFile(cacheFile, page, "UTF-8");
+      } catch (Exception ex) {
+         // TODO(eriq): Log Error
+         System.err.println("Error creating cache.");
+         cacheFile.delete();
+      }
+   }
+
+   private static String fetchCache(File cacheFile) {
+      try {
+         if (!cacheFile.exists()) {
+            return null;
+         }
+
+         return FileUtils.readFileToString(cacheFile, "UTF-8");
+      } catch (Exception ex) {
+         // TODO(eriq): Log Error
+         System.err.println("Error fetching cache.");
+         return null;
+      }
+   }
+}
